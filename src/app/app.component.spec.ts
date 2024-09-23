7291dff14ff586132d412df2a8fd7de5728be48f
rch850 <rich850@gmail.com>
2024-09-23 15:46:18 +0900

    🆙 Update Angular 10 to 11
    ```
    ❯ ng update @angular/core@11 @angular/cli@11
    The installed Angular CLI version is outdated.
    Installing a temporary Angular CLI versioned 11.2.19 to perform the update.
    Installing packages for tooling via yarn.
    Installed packages for tooling via yarn.
    Using package manager: 'yarn'
    Collecting installed dependencies...
    Found 34 dependencies.
    Fetching dependency metadata from registry...
        Updating package.json with dependency @angular-devkit/build-angular @ "0.1102.19" (was "0.1002.4")...
        Updating package.json with dependency @angular/cli @ "11.2.19" (was "10.2.4")...
        Updating package.json with dependency @angular/compiler-cli @ "11.2.14" (was "10.2.5")...
        Updating package.json with dependency karma @ "6.4.4" (was "5.0.9")...
        Updating package.json with dependency @angular/animations @ "11.2.14" (was "10.2.5")...
        Updating package.json with dependency @angular/common @ "11.2.14" (was "10.2.5")...
        Updating package.json with dependency @angular/compiler @ "11.2.14" (was "10.2.5")...
        Updating package.json with dependency @angular/core @ "11.2.14" (was "10.2.5")...
        Updating package.json with dependency @angular/forms @ "11.2.14" (was "10.2.5")...
        Updating package.json with dependency @angular/platform-browser @ "11.2.14" (was "10.2.5")...
        Updating package.json with dependency @angular/platform-browser-dynamic @ "11.2.14" (was "10.2.5")...
        Updating package.json with dependency @angular/platform-server @ "11.2.14" (was "10.2.5")...
        Updating package.json with dependency @angular/router @ "11.2.14" (was "10.2.5")...
      UPDATE package.json (1444 bytes)
    ✔ Packages installed successfully.
    ** Executing migrations of package '@angular/cli' **
    
    ❯ Replace deprecated library builder '@angular-devkit/build-ng-packagr'.
      Migration completed.
    
    ❯ Add 'declarationMap' compiler options for non production library builds.
      Migration completed.
    
    ❯ Remove deprecated options from 'angular.json' that are no longer present in v11.
      UPDATE angular.json (3748 bytes)
      Migration completed.
    
    ❯ Update workspace dependencies to match a new v11 project.
      UPDATE package.json (1444 bytes)
    ✔ Packages installed successfully.
      Migration completed.
    
    a** Executing migrations of package '@angular/core' **
    
    ❯ In Angular version 11, the type of `AbstractControl.parent` can be `null` to reflect the runtime value more accurately.
      This migration automatically adds non-null assertions to existing accesses of the `parent` property on types like `FormControl`, `FormArray` and `FormGroup`.
    ^R
      Migration completed.
    
    ❯ ViewEncapsulation.Native has been removed as of Angular version 11.
      This migration replaces any usages with ViewEncapsulation.ShadowDom.
      Migration completed.
    
    ❯ NavigationExtras omissions migration.
      In version 11, some unsupported properties were omitted from the `extras` parameter of the `Router.navigateByUrl` and `Router.createUrlTree` methods.
      Migration completed.
    
    ❯ Updates the `initialNavigation` property for `RouterModule.forRoot`.
      Migration completed.
    
    ❯ NavigationExtras.preserveQueryParams has been removed as of Angular version 11.
       This migration replaces any usages with the appropriate assignment of the queryParamsHandling key.
      Migration completed.
    
    ❯ The default value for `relativeLinkResolution` is changing from 'legacy' to 'corrected'.
    This migration updates `RouterModule` configurations that use the default value to
    now specifically use 'legacy' to prevent breakages when updating.
      Migration completed.
    
    ❯ `async` to `waitForAsync` migration.
      The `async` testing function has been renamed to `waitForAsync` to avoid confusion with the native `async` keyword.
      UPDATE src/app/app.component.spec.ts (1181 bytes)
      Migration completed.
    
    ❯ Removes `canActivate` from a `Route` config when `redirectTo` is also present.
      Migration completed.
    ```

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,10 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from 'app/shared/shared.module';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
         SharedModule
@@ -15,19 +15,19 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the app', async(() => {
+  it('should create the app', waitForAsync(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title '出るまで回すやつ'`, async(() => {
+  it(`should have as title '出るまで回すやつ'`, waitForAsync(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('出るまで回すやつ');
   }));
 
-  it('should render title in a h1 tag', async(() => {
+  it('should render title in a h1 tag', waitForAsync(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
